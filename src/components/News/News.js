aabbb927d556d85e35f62534fee430fa1ac6126b
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-07-16 01:00:16 +0300

    add sort

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,6 +1,8 @@
 import { Avatar, Card, Col, Row, Select, Typography } from "antd"
 import moment from "moment"
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import { useGetCryptosQuery } from "../../services/cryptoApi"
 import { useGetCryptoNewsQuery } from "../../services/newsApi"
 
 const { Text, Title } = Typography
@@ -9,7 +11,9 @@ const { Option } = Select
 const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
 function News({ simplified }) {
-    const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 21 })
+    const [newsCategory, setNewsCategory] = useState("Cryptocurrency")
+    const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 21 })
+    const { data } = useGetCryptosQuery(100)
 
     console.log(cryptoNews)
 
@@ -19,6 +23,23 @@ function News({ simplified }) {
 
     return (
         <Row gutter={[24, 24]}>
+            {!simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="select-news"
+                        placeholder="Slect a Crypto"
+                        optionFilterProp="children"
+                        onChange={(value) => setNewsCategory(value)}
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLocaleLowerCase()) >= 0}
+                    >
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        {data?.data?.coins.map((item) => (
+                            <Option value={item.name}>{item.name}</Option>
+                        ))}
+                    </Select>
+                </Col>
+            )}
             {cryptoNews?.value.map((item, index) => (
                 <Col xs={24} sm={12} lg={8} key={index}>
                     <Card hoverable className="news-card">
