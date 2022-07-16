068fd19bf20114812afa497758b23fbba37825ec
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-07-16 17:05:14 +0300

    add line chart

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -1,4 +1,5 @@
 import { Col, Row, Typography } from "antd"
+import { Chart as ChartJS } from 'chart.js/auto'
 import { Line } from "react-chartjs-2"
 
 const { Title } = Typography
@@ -9,34 +10,32 @@ function LineChart({ coinHistory, currentPrice, coinName }) {
 
     for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
         coinPrice.push(coinHistory?.data?.history[i].price);
-    }
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
         coinTimeStamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
     }
-    // const data = {
-    //     labels: coinTimeStamp,
-    //     datasets: [
-    //         {
-    //             label: 'Price In USD',
-    //             data: coinPrice,
-    //             fill: false,
-    //             backgroundColor: '#0071bd',
-    //             borderColor: '#0071bd',
-    //         },
-    //     ],
-    // };
+    const data = {
+        labels: coinTimeStamp,
+        datasets: [
+            {
+                label: 'Price In USD',
+                data: coinPrice,
+                fill: false,
+                backgroundColor: '#0071bd',
+                borderColor: '#0071bd',
+            },
+        ],
+    };
 
-    // const options = {
-    //     scales: {
-    //         yAxes: [
-    //             {
-    //                 ticks: {
-    //                     beginAtZero: true,
-    //                 },
-    //             },
-    //         ],
-    //     },
-    // };
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    };
     return (
         <>
             <Row className="chart-header">
@@ -52,10 +51,7 @@ function LineChart({ coinHistory, currentPrice, coinName }) {
                     </Title>
                 </Col>
             </Row>
-            {/* <Line
-                options={options}
-                data={data}
-            /> */}
+            <Line data={data} options={options} />
         </>
     )
 }
